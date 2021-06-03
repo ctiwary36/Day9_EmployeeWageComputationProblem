0b0788ef55cc179322e54ba1c1b52df77bab5778
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-03 13:00:14 +0530

    Added:uc1-Check Employee is present or Absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Employee Wage Computation Program"
+
+
+isPresent=1
+
+
+randomNumber=$((RANDOM%2))
+
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
